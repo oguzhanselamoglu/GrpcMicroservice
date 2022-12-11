b372d4622a62cca64c6737bafac7bae5f0c0fc1b
oguzhan <oguzhanselamoglu@outlook.com>
2022-12-11 17:19:24 +0300

    Update Program.cs

--- ProductGrpcClient/Program.cs
+++ ProductGrpcClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
 using Grpc.Net.Client;
 using ProductGrpc.Protos;
 
@@ -6,22 +7,44 @@ Console.WriteLine("Hello, World!");
 Thread.Sleep(2000);
 using var channel = GrpcChannel.ForAddress("http://localhost:5286");
 var client = new ProductProtoService.ProductProtoServiceClient(channel);
-Console.WriteLine("GetproductAsync started..");
-var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
-Console.WriteLine($"GetProductAsync Response :{response.ToString()}");
+await GetProductAsync(client);
+await GetAllProductAsync(client);
 
 
 
-//GetAllProducts
-Console.WriteLine("GetAllProducts started..");
 
-using var clientData = client.GetAllProducts(new GetAllProductsRequest());
-while (await clientData.ResponseStream.MoveNext(new CancellationToken()))
+
+
+async Task GetProductAsync(ProductProtoService.ProductProtoServiceClient client)
 {
+    Console.WriteLine("GetproductAsync started..");
+    var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
+    Console.WriteLine($"GetProductAsync Response :{response.ToString()}");
 
-    var currentProduct = clientData.ResponseStream.Current;
-    Console.WriteLine(currentProduct);
 }
+
+async Task GetAllProductAsync(ProductProtoService.ProductProtoServiceClient client)
+{
+    //GetAllProducts
+    Console.WriteLine("GetAllProducts started..");
+
+    //using var clientData = client.GetAllProducts(new GetAllProductsRequest());
+    //while (await clientData.ResponseStream.MoveNext(new CancellationToken()))
+    //{
+
+    //    var currentProduct = clientData.ResponseStream.Current;
+    //    Console.WriteLine(currentProduct);
+    //}
+
+  
+    using var clientData = client.GetAllProducts(new GetAllProductsRequest());
+    await foreach (var responseData in clientData.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(responseData);
+    }
+
+}
+
 Console.ReadKey();
 
 
