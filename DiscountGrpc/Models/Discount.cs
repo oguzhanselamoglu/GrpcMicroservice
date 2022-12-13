06eee8ecd4195ab46e53d94a0e53e1bdf9541163
oguzhan <oguzhanselamoglu@outlook.com>
2022-12-13 14:11:50 +0300

    Added DiscountGrpc

---
+++ DiscountGrpc/Models/Discount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DiscountGrpc.Models
+{
+	public class Discount
+	{
+        public int DiscountId { get; set; }
+        public string Code { get; set; }
+        public int Amount { get; set; }
+    }
+}
+
---
+++ DiscountGrpc/Services/DiscountService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DiscountGrpc.Data;
+using DiscountGrpc.Protos;
+using Grpc.Core;
+
+namespace DiscountGrpc.Services
+{
+	public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+	{
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(ILogger<DiscountService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<DiscountModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var discount = DiscountContext.Discounts.FirstOrDefault(d => d.Code == request.DiscountCode);
+
+            _logger.LogInformation("Discount is operated with the {discountCode} code and the amount is : {discountAmount}", discount.Code, discount.Amount);
+
+            return Task.FromResult(new DiscountModel
+            {
+                DiscountId = discount.DiscountId,
+                Code = discount.Code,
+                Amount = discount.Amount
+            });
+        }
+
+    }
+}
+
