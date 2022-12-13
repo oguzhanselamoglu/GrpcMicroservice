08f61ef36a47ba9a55a72bef44f01214007385eb
oguzhan <oguzhanselamoglu@outlook.com>
2022-12-13 12:38:58 +0300

    Added ShppingCardGrpc

---
+++ ProductWorkerService/ProductFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Google.Protobuf.WellKnownTypes;
+using ProductGrpc.Protos;
+
+namespace ProductWorkerService
+{
+	public class ProductFactory
+	{
+        private readonly ILogger<ProductFactory> _logger;
+        private readonly IConfiguration _configuration;
+
+        public ProductFactory(ILogger<ProductFactory> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public Task<AddProductRequest> Generate()
+        {
+            var productName = $"{_configuration.GetValue<string>("WorkerService:ProductName")}_{DateTimeOffset.Now}";
+            var productRequest = new AddProductRequest
+            {
+                Product = new ProductModel
+                {
+                    Name = productName,
+                    Description = $"{productName}_Description",
+                    Price = new Random().Next(1000),
+                    Status = ProductStatus.Instock,
+                    CreatedTime = Timestamp.FromDateTime(DateTime.UtcNow)
+                }
+            };
+
+            return Task.FromResult(productRequest);
+        }
+    }
+}
+
--- ProductWorkerService/Worker.cs
+++ ProductWorkerService/Worker.cs
@@ -7,11 +7,13 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IConfiguration _configuration;
+    private readonly ProductFactory _productFactory;
 
-    public Worker(ILogger<Worker> logger, IConfiguration configuration)
+    public Worker(ILogger<Worker> logger, IConfiguration configuration, ProductFactory productFactory)
     {
         _logger = logger;
         _configuration = configuration;
+        _productFactory = productFactory;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,11 +24,14 @@ public class Worker : BackgroundService
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
         
-            using var channel = GrpcChannel.ForAddress("http://localhost:5286");
+            using var channel = GrpcChannel.ForAddress(_configuration.GetValue<string>("WorkerService:ServerUrl"));
             var client = new ProductProtoService.ProductProtoServiceClient(channel);
-            Console.WriteLine("GetproductAsync started..");
-            var response = await client.GetProductAsync(new GetProductRequest { ProductId = 1 });
-            Console.WriteLine($"GetProductAsync Response :{response.ToString()}");
+
+            _logger.LogInformation("AddProductAsync Started..");
+            var addProductResponse = await client.AddProductAsync(await _productFactory.Generate());
+            _logger.LogInformation("AddProduct response:{product} ", addProductResponse.ToString());
+            
+            
 
 
             await Task.Delay(_configuration.GetValue<int>("WorkerService:TaskInterval"), stoppingToken);
