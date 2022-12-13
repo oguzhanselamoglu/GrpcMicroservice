bae6d2addcfdd142653174ee40389be2c5e6afa8
oguzhan <oguzhanselamoglu@outlook.com>
2022-12-13 17:29:25 +0300

    Shoping Cart Worker service added for consuming Shopping Cart

---
+++ ShopCartWorkerService/Program.cs
@@ -0,0 +1,7 @@
+using ShopCartWorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+host.Run();
